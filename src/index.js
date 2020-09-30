9a3569a86dac5faa3513d22afcd4bc767679d373
Artem <69482749+ArtemkaOffical@users.noreply.github.com>
2020-09-30 08:41:45 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let str = n.toString();
+  str = str.split('').reverse().join('');
+  if(str.includes('-')){str = str.replace(/-/,'')}
+  return Number(str);
 }
